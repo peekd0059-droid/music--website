e98a23aa901b4707fa60ebf306ca1b889d4729c1
Harsh <peekd0059@gmail.com>
2026-04-20 16:30:58 +0530

    FINAL SPOTIFY APP

--- app.py
+++ app.py
@@ -19,54 +19,21 @@ def home():
     return render_template("index.html", songs=songs)
 
 
-# LOGIN
-@app.route('/login', methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+# 🔍 SEARCH
+@app.route('/search')
+def search():
+    query = request.args.get("q")
 
-        conn = get_db()
-        c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username,password))
-        user = c.fetchone()
-        conn.close()
+    conn = get_db()
+    c = conn.cursor()
+    c.execute("SELECT * FROM songs WHERE name LIKE ?",('%'+query+'%',))
+    songs = c.fetchall()
+    conn.close()
 
-        if user:
-            session["user"] = username
-            return redirect('/')
-        else:
-            return "Wrong Login"
-
-    return render_template("login.html")
+    return render_template("index.html", songs=songs)
 
 
-# SIGNUP
-@app.route('/signup', methods=["GET","POST"])
-def signup():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        conn = get_db()
-        c = conn.cursor()
-        c.execute("INSERT INTO users (username,password) VALUES (?,?)",(username,password))
-        conn.commit()
-        conn.close()
-
-        return redirect('/login')
-
-    return render_template("signup.html")
-
-
-# LOGOUT
-@app.route('/logout')
-def logout():
-    session.pop("user",None)
-    return redirect('/')
-
-
-# 🔥 LIKE SONG
+# ❤️ LIKE
 @app.route('/like/<int:song_id>')
 def like(song_id):
     username = session.get("user","guest")
@@ -80,7 +47,7 @@ def like(song_id):
     return redirect('/')
 
 
-# 🔥 LIKED PAGE
+# ❤️ LIKED PAGE
 @app.route('/liked')
 def liked():
     username = session.get("user","guest")
@@ -98,21 +65,7 @@ def liked():
     return render_template("index.html", songs=songs)
 
 
-# 🔥 SEARCH
-@app.route('/search')
-def search():
-    query = request.args.get("q")
-
-    conn = get_db()
-    c = conn.cursor()
-    c.execute("SELECT * FROM songs WHERE name LIKE ?",('%'+query+'%',))
-    songs = c.fetchall()
-    conn.close()
-
-    return render_template("index.html", songs=songs)
-
-
-# 🔥 CREATE PLAYLIST
+# 📂 PLAYLIST
 @app.route('/create_playlist', methods=["POST"])
 def create_playlist():
     name = request.form["name"]
