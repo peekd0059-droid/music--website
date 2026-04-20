9b381491710f3c4712960fecaf84805b2d2a3d10
Harsh <peekd0059@gmail.com>
2026-04-20 16:55:58 +0530

    like toggle system added

--- app.py
+++ app.py
@@ -10,10 +10,20 @@ app.secret_key = "secret123"
 def home():
     conn = get_db()
     c = conn.cursor()
+
     c.execute("SELECT * FROM songs")
     songs = c.fetchall()
+
+    username = session.get("user", "guest")
+
+    # GET LIKED SONG IDS
+    c.execute("SELECT song_id FROM likes WHERE username=?", (username,))
+    liked = c.fetchall()
+    liked_ids = [i[0] for i in liked]
+
     conn.close()
-    return render_template("index.html", songs=songs)
+
+    return render_template("index.html", songs=songs, liked_ids=liked_ids)
 
 
 # SEARCH
@@ -23,64 +33,85 @@ def search():
 
     conn = get_db()
     c = conn.cursor()
-    c.execute("SELECT * FROM songs WHERE name LIKE ?",('%'+query+'%',))
+    c.execute("SELECT * FROM songs WHERE name LIKE ?", ('%' + query + '%',))
     songs = c.fetchall()
+
+    username = session.get("user", "guest")
+
+    c.execute("SELECT song_id FROM likes WHERE username=?", (username,))
+    liked = c.fetchall()
+    liked_ids = [i[0] for i in liked]
+
     conn.close()
 
-    return render_template("index.html", songs=songs)
+    return render_template("index.html", songs=songs, liked_ids=liked_ids)
 
 
-# LIKE
+# ❤️ LIKE TOGGLE
 @app.route('/like/<int:song_id>')
 def like(song_id):
-    username = session.get("user","guest")
+    username = session.get("user", "guest")
 
     conn = get_db()
     c = conn.cursor()
-    c.execute("INSERT INTO likes (username, song_id) VALUES (?,?)",(username,song_id))
+
+    # CHECK EXIST
+    c.execute("SELECT * FROM likes WHERE username=? AND song_id=?", (username, song_id))
+    existing = c.fetchone()
+
+    if existing:
+        # UNLIKE
+        c.execute("DELETE FROM likes WHERE username=? AND song_id=?", (username, song_id))
+    else:
+        # LIKE
+        c.execute("INSERT INTO likes (username, song_id) VALUES (?, ?)", (username, song_id))
+
     conn.commit()
     conn.close()
 
     return redirect('/')
 
 
-# LIKED
+# ❤️ LIKED PAGE
 @app.route('/liked')
 def liked():
-    username = session.get("user","guest")
+    username = session.get("user", "guest")
 
     conn = get_db()
     c = conn.cursor()
+
     c.execute("""
     SELECT songs.* FROM songs
     JOIN likes ON songs.id = likes.song_id
     WHERE likes.username=?
-    """,(username,))
+    """, (username,))
     songs = c.fetchall()
+
+    liked_ids = [s[0] for s in songs]
+
     conn.close()
 
-    return render_template("index.html", songs=songs)
+    return render_template("index.html", songs=songs, liked_ids=liked_ids)
 
 
-# CREATE PLAYLIST
+# PLAYLIST
 @app.route('/create_playlist', methods=["POST"])
 def create_playlist():
     name = request.form["name"]
-    username = session.get("user","guest")
+    username = session.get("user", "guest")
 
     conn = get_db()
     c = conn.cursor()
-    c.execute("INSERT INTO playlists (username,name) VALUES (?,?)",(username,name))
+    c.execute("INSERT INTO playlists (username,name) VALUES (?,?)", (username, name))
     conn.commit()
     conn.close()
 
     return redirect('/')
 
 
-# SHOW PLAYLIST
 @app.route('/playlist')
 def playlist():
-    username = session.get("user","guest")
+    username = session.get("user", "guest")
 
     conn = get_db()
     c = conn.cursor()
